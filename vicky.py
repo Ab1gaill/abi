27959c8980404d9123ef5a57ca8c2868a44a243b
Ab1gaill <a.victoriaarcari@icea.edu.ar>
2024-10-01 19:23:53 +0000

    h

--- vicky.py
+++ vicky.py
@@ -1,1 +1,15 @@
-print("Gato1")
+x=0
+y=0
+def arriba():
+   return True
+while True:
+    if arriba():
+        y=y-1
+    if abajo():
+       y=y+1
+    if derecha():
+        x=x+1
+    if izquierda():
+         x=x-1
+    if 
+     
